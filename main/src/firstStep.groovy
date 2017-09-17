5b3edecbd8c3a09b485a4374b31c7d0884af346b
chroman78 <chroman78@users.noreply.github.com>
2017-09-17 22:37:01 +0300

    commit

--- main/src/firstStep.groovy
+++ main/src/firstStep.groovy
@@ -1,1 +1,23 @@
-println('Проверка')
+
+
+def sss=["первый",2,'третий']
+
+println(sss[1])
+println(sss[2])
+
+sss << "xtndthnsq"
+println(sss[3])
+println(sss[4])
+
+println("размер списка = " + sss.size())
+
+for (e in sss){
+    println(e)
+}
+
+def amPM = Calendar.getInstance().get(Calendar.AM_PM)
+if (amPM == Calendar.AM){
+    println("Good morning")
+} else {
+    println("Good evening")
+}
