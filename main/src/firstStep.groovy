f30637abd725cadf4f239a5b44126a44a88ae5a8
chroman78 <chroman78@users.noreply.github.com>
2018-01-07 14:43:35 +0300

    1
    1

--- main/src/firstStep.groovy
+++ main/src/firstStep.groovy
@@ -1,19 +1,45 @@
+//класс пользователя
+class Person{
+    String first, last
+}
+//функции без типизированных параметров
+def printInfo(first, second){
+    println "first name: $first, second name: $second"
+}
+def printFirstName(user){
+    println "user: $user.first"
+}
+//создаем обьект класса Person с параметрами
+def tempPerson = new Person(first: 'Adam', last: 'Smith')
 
-javaString = 'Проверка 1'
-groovyString = "Ghjdthrf ${javaString} 2"
 
-println(javaString)
-println(groovyString)
+// вызов функции разными способами
 
-bigGroovyString = """
-  long
-   long
-    string
-""" // Длинная строка с отступами
 
-println(bigGroovyString)
+/*
+printInfo tempPerson.first, tempPerson.last
 
-def a = "a"
-println a
-println a + "12" // конкатенация
-println a * 3 // умножение
+printFirstName(tempPerson)
+
+printFirstName tempPerson
+
+a = "0123456789"
+println(a[1..4])
+
+year = 100
+
+if (year<4) b = 0;
+if ((year%4 == 0 & year%100 !=0) || year%400) b = year/4;
+else if (year%4 == 0 & year%100 == 0) b = year/4-1;
+
+c = '\u03A9'
+println(c)
+
+
+println(b)*/
+
+double a = 0.1;
+double b = 0.2;
+int i = 23;
+
+println((char)((int)'\\'+ i))
